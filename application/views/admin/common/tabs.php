c714f4539228e30c170f7eff0495b85dda3c10af
Palini Pulusu <ppulusu92@gmail.com>
2018-04-05 10:37:46 -0500

    removed dc invoice tab

--- application/views/admin/common/tabs.php
+++ application/views/admin/common/tabs.php
@@ -1,7 +1,5 @@
 <ul>
     <li <?php if ($_controller == 'SupportRequest'):?>id="selected_tab"<?php endif; ?>><a href="<?php echo base_url();?>admin/SupportRequest/index">Enter Support Request</a></li>
-    <li <?php if ($_controller == 'WasteInvoice'):?>id="selected_tab"<?php endif; ?>><a href="<?php echo base_url();?>admin/WasteInvoice/Add">Enter N. Miss. Invoice</a></li>
-	<!-- <li <?php if ($_controller == 'DCInvoice'):?>id="selected_tab"<?php endif; ?>><a href="<?php echo base_url();?>admin/DCInvoice/Add">Enter Penn Medicine Invoice</a></li>
- -->    
+    <li <?php if ($_controller == 'WasteInvoice'):?>id="selected_tab"<?php endif; ?>><a href="<?php echo base_url();?>admin/WasteInvoice/Add">Enter Invoice</a></li>  
  	<li <?php if ($_controller == 'RecyclingInvoice'):?>id="selected_tab"<?php endif; ?>><a href="<?php echo base_url();?>admin/RecyclingInvoice/AddEdit">Enter Recycling PO</a></li>
 </ul>
